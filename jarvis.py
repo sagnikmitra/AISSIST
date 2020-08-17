f99fc6743956c5c1117012b86503d70b434f0095
Sagnik Mitra <sagnikmitra123@gmail.com>
2020-08-17 15:57:13 +0530

    commit on master

--- jarvis.py
+++ jarvis.py
@@ -116,4 +116,4 @@ if __name__ == "__main__":
                 speak("Email has been sent!")
             except Exception as e:
                 print(e)
-                speak("Sorry Sagnik, I am unable to send this email")
+                speak("Sorry Sagnik, I am unable to send the email")
