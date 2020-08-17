d3ce86c1770af9f972650388cba252b1b3b939dc
Sagnik Mitra <sagnikmitra123@gmail.com>
2020-08-17 18:30:09 +0530

    commit on master

--- jarvis.py
+++ jarvis.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
             webbrowser.open("stackoverflow.com")
 
         elif 'play music' in query:
-            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+            music_dir = 'D:\\MEDIA\\MUSIC'
             songs = os.listdir(music_dir)
             print(songs)
             os.startfile(os.path.join(music_dir, songs[0]))
